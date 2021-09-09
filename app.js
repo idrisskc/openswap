a698a7a78f34ccf34ff6f68dc59ebd009a8de655
ExcelDurant <gnopaexcel@yahoo.fr>
2021-09-09 17:38:36 +0100

    installed MySql, configured the mySql connection, installed dotenv and modified the env file for the database connection

--- app.js
+++ app.js
@@ -2,10 +2,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('mysql');
 
 var apiRouter = require('./routes/api');
 
-require('dotenv').config()
+require('dotenv').config({ path: '.env' });
+
+
+// creates a connection to the local MySql database
+var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
+// creates a new connection to the db
+connection.connect(()=> {
+    console.log('database connected succesfully');
+});
 
 var app = express();
 
